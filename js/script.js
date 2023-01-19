51a2ae9e5d0cfb1b291efbcaa019f97179e39d31
ferZyx <sorokin.wlad@yandex.ru>
2023-01-19 11:33:58 +0600

    experiments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 
 let tg = window.Telegram.WebApp;
 
-document.write(tg.initDataUnsafe.user.id)
+let data = { id: tg.initDataUnsafe.user.id }
+
+fetch('https://tolyan.onrender.com/get_schedule_for_bot_by_id/', {
+  method: 'POST',
+  body: JSON.stringify(data), 
+  headers: { 'Content-Type': 'application/json' },
+})
+.then(response => response.json())
+.then(data => document.write(data))
+.catch(error => document.write('Error:', error));
+
 
