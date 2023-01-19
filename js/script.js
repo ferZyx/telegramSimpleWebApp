bacdfd36df6d802995b8d4382bc8089fc29a4158
ferZyx <sorokin.wlad@yandex.ru>
2023-01-19 13:17:15 +0600

    it is?

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let main_table = document.querySelector('#main_table')
 let day_dictionary = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 let tg = window.Telegram.WebApp;
-let data = { user_id: tg.initDataUnsafe.user.id }
+// let data = { user_id: tg.initDataUnsafe.user.id }
+let data = { user_id: 638404076 }
 let table = ''
 
 fetch('https://tolyan.onrender.com/get_schedule_for_bot_by_id', {
@@ -16,7 +17,7 @@ fetch('https://tolyan.onrender.com/get_schedule_for_bot_by_id', {
             table += '<td>' + day_dictionary[i] + '</td>'
             schedule_day_array = data[i].split('\n\n')
             schedule_day_array.forEach(element => {
-                table += '<td>' + element + '</td>'
+                table += '<td>' + element.replace('\n', '<br>') + '</td>'
             });
             table += '</tr>'
         }
