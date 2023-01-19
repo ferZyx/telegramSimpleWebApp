3a7dfa33bf5bc00393b0409f15f26e4da5818e5f
ferZyx <sorokin.wlad@yandex.ru>
2023-01-19 18:09:35 +0600

    added preloader

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 let main_table = document.querySelector('#main_table')
 let day_dictionary = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 let tg = window.Telegram.WebApp;
-tg.expand()
-let data = { user_id: tg.initDataUnsafe.user.id }
-// let data = { user_id: 638404076 }
+// let data = { user_id: tg.initDataUnsafe.user.id }
+let data = { user_id: 638404076 }
 let table = ''
 let today = new Date();
 let dayOfWeek = today.getDay() - 1;
+
 if(dayOfWeek < 0) {
     dayOfWeek = 0
+}
+
+tg.expand()
+
+function load_page(){
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
 }
 
 fetch('https://tolyan.onrender.com/get_schedule_for_bot_by_id', {
@@ -30,8 +40,13 @@ fetch('https://tolyan.onrender.com/get_schedule_for_bot_by_id', {
         main_table.innerHTML = table
         tr_list = document.querySelectorAll('tr')
         tr_list[dayOfWeek].scrollIntoView({ behavior: 'smooth' });
-
+        load_page()
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+         console.error('Error:', error)
+         main_table.innerHTML = 'А ты говорил мне какое тебе нужно расписание?'
+         load_page()
+        });
+
 
 
