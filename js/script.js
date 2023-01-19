d22a1736d3ac21a237de3daead90a47f433b27d5
S-Selfish <artemushkats@gmail.com>
2023-01-19 20:29:43 +0600

    feat: add blink animation

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 let main_table = document.querySelector('#main_table')
 let day_dictionary = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 let tg = window.Telegram.WebApp;
-let data = { user_id: tg.initDataUnsafe.user.id }
-// let data = { user_id: 638404076 }
+//let data = { user_id: tg.initDataUnsafe.user.id }
+let data = { user_id: 63840076 }
 let table = ''
 let today = new Date();
 let dayOfWeek = today.getDay() - 1;
+let errorMsg = document.querySelector('#error-msg');
 
-if(dayOfWeek < 0) {
+if (dayOfWeek < 0) {
     dayOfWeek = 0
 }
 
 tg.expand()
 
-function load_page(){
+function load_page() {
     document.body.classList.add('loaded_hiding');
     window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
     }, 500);
 }
 
@@ -43,10 +44,28 @@ fetch('https://tolyan.onrender.com/get_schedule_for_bot_by_id', {
         load_page()
     })
     .catch(error => {
-         console.error('Error:', error)
-         load_page()
-         document.querySelector('#error-msg').innerHTML = 'Произошла ошибка. Попробуйте загрузить расписание через бота, и потом попробовать снова.'
-        });
+        console.error('Error:', error)
+        load_page()
+        console.log("Fuck")
+        typewriter(errorMsg, "Произошла ошибка. Попробуйте загрузить расписание через бота, и потом попробовать снова.");
+    });
+
+
+function typewriter(error, string) {
+    let i = 0;
+    const speed = 30;
+
+    function type() {
+        if (i < string.length) {
+            error.innerHTML += string.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+
 
 
 
